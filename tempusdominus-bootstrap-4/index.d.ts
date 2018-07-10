61731ad0ec7cc8ddd99a0c695ad298f555fd53bf
kukjevov <kukjevov@gmail.com>
2018-07-10 09:40:36 +0200

    UPDATE - added tempus dominus typings

---
+++ tempusdominus-bootstrap-4/index.d.ts
@@ -0,0 +1,7 @@
+///<reference types="jquery" />
+///<reference types="eonasdan-bootstrap-datetimepicker" />
+
+export as namespace TempusDominusDatetimepicker;
+
+import 'eonasdan-bootstrap-datetimepicker';
+export * from 'eonasdan-bootstrap-datetimepicker';
